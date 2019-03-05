363a776aeecea5e13659e9d0928c25cb4eb3c8bf
Bryan Bishop <bryan.bishop@pearson.com>
2019-03-04 22:52:34 -0600

    Add requirements :green_heart:

--- test_chatbot.py
+++ test_chatbot.py
@@ -1,14 +1,16 @@
 import chatbot
 
 
-def test_names_game():
+def test_names():
     results = chatbot.name_game("Amanda").splitlines()
     assert results[0] == "amanda, amanda, bo-bamanda"
+    
     results = chatbot.name_game("Stan").splitlines()
     assert results[0] == "stan, stan, bo-ban"
+    
     results = chatbot.name_game("Bryan").splitlines()
     assert results[0] == "bryan, bryan, bo-byan"
 
-def test_swear_word():
+def test_no_swearing():
     results = chatbot.name_game("Chuck").splitlines()
     assert results[2] == "Hey, you're trying to trick me!"
