ece12502359b731d21c243b883e35407e44cf9e2
Bryan Bishop <bryan.bishop@pearson.com>
2019-03-05 23:58:41 -0600

    Add some simple tests :fire:

--- test_chatbot.py
+++ test_chatbot.py
@@ -1,6 +1,15 @@
 import chatbot
 
 def test_affirmative_responses():
-    r, next_fun = chatbot.affirmative_responses()
-    assert r == 'a' or r == 'b' or r == 'c'
-    assert callable(next_fun) == True
+    assert chatbot.affirmative_responses('yes')
+    assert chatbot.affirmative_responses('SURE')
+    assert chatbot.affirmative_responses('oK')
+    assert not chatbot.affirmative_responses('no')
+    assert not chatbot.affirmative_responses('')
+
+def test_chat_prompts():
+    for count in range(100):
+        response = chatbot.chat_prompts()
+        assert list(response.keys())[0].__class__ == str
+        assert callable(list(response.values())[0])
+    
